e7eb25be223664508cfad5b493e46bb36f2484d9
kaminora <kaminoura9207@gmail.com>
2019-06-22 20:21:00 +0900

    Add task using take latest

--- src/sagas.js
+++ src/sagas.js
@@ -2,15 +2,19 @@ import { eventChannel } from 'redux-saga';
 import { call, delay, fork, takeLatest, put } from 'redux-saga/effects';
 import { addArea, removeArea } from './actions';
 
-function* addCounter() {
+function* takeLatestTask() {
    try {
-      yield call(delay, 1000 + Math.random() * 2000);
+      console.log('takeLatest task: start')
+      const startTime = new Date()
+      yield delay(2000);
       yield put({type: "ADD_COUNTER"});
+      const finishTime = new Date()
+      console.log('takeLatest task: done', finishTime - startTime )
    } catch (e) {
+      console.log('error:', e)
    }
 }
 
 export default function* rootSaga() {
-  // yield fork(areaWatcher);
-  yield takeLatest(addArea, addCounter)
+  yield takeLatest(addArea, takeLatestTask)
 }
